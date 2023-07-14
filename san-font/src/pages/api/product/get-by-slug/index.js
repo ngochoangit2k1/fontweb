ca3dcf363c021886909576540f515c59f99aa34f
ngochoangit2k1 <59520070+ngochoangit2k1@users.noreply.github.com>
2023-07-14 11:13:32 +0700

    update get product

--- san-font/src/pages/api/product/get-by-slug/index.js
+++ san-font/src/pages/api/product/get-by-slug/index.js
@@ -1,44 +1,32 @@
 import db from "../../../../backend/models/index";
-import {GLOBAL_SWITCH, GLOBAL_STATUS} from "../../../../backend/constants/common.constant"
+import {
+  GLOBAL_SWITCH,
+  GLOBAL_STATUS,
+} from "../../../../backend/constants/common.constant";
 import { HTTP_ERROR, FIELD_ERROR } from "../../../../backend/errors/error";
-
+import { where } from "sequelize";
 
 export default async function handle(req, res, next) {
   if (req.method === "GET") {
-    const { productSlug, getMainImage } = req.query;
-    console.log( req.query)
-    const conditions = {
-      status: GLOBAL_STATUS.ACTIVE,
-    };
+    const { categorySlug, getMainImage } = req.query;
+    const category = await db.ProductCategory.findOne({
+      where: { categorySlug },
+      attributes: ["id"],
+    });
+    console.log("checkc",category.id)
+  
 
-    productSlug && (conditions.productSlug = productSlug);
-
-    const products = await db.Product.findOne({
-      where: conditions,
+    const products = await db.Product.findAndCountAll({
+      where: {  categoryId: category.id },
       include: [
         {
           model: db.ProductCategory,
-          as: "productCategory",
+          as: 'productCategory',
           where: { status: GLOBAL_STATUS.ACTIVE },
-        },
-        {
-          model: db.ProductInventory,
-          as: "productInventory",
-        },
-        {
-          model: db.Discount,
-          as: "discount",
-          required: false,
-          where: { status: GLOBAL_STATUS.ACTIVE },
-        },
-        {
-          model: db.ProductDetail,
-          as: "productDetail",
-          separate: true,
         },
         {
           model: db.ProductImage,
-          as: "productImage",
+          as: 'productImage',
           separate: true,
           where: {
             isMain: getMainImage
@@ -46,6 +34,19 @@ export default async function handle(req, res, next) {
               : [GLOBAL_SWITCH.ON, GLOBAL_SWITCH.OFF],
           },
         },
+        {
+          model: db.ProductInventory,
+          as: 'productInventory',
+          separate: true,
+        },
+
+        // {
+        //   model: db.Discount,
+        //   as: 'discount',
+        //   required: !!discount,
+        //   where: { status: GLOBAL_STATUS.ACTIVE },
+        // },
+        { model: db.User, as: 'user' },
       ],
     });
 
@@ -57,6 +58,50 @@ export default async function handle(req, res, next) {
       });
     }
 
-    return res.status(200).json( products);
+    return res.status(200).json(products);
+  }
+  if (req.method === "POST") {
+
+  
+
+    const products = await db.Product.findAndCountAll({
+      where: {  categoryId: req.body.categoryId },
+      include: [
+        {
+          model: db.ProductCategory,
+          as: 'productCategory',
+          where: { status: GLOBAL_STATUS.ACTIVE },
+        },
+        {
+          model: db.ProductImage,
+          as: 'productImage',
+          separate: true,
+         
+        },
+        {
+          model: db.ProductInventory,
+          as: 'productInventory',
+          separate: true,
+        },
+
+        // {
+        //   model: db.Discount,
+        //   as: 'discount',
+        //   required: !!discount,
+        //   where: { status: GLOBAL_STATUS.ACTIVE },
+        // },
+        { model: db.User, as: 'user' },
+      ],
+    });
+
+    if (!products) {
+      return res.status(HTTP_ERROR.BAD_REQUEST).json({
+        name: "get_product",
+        code: FIELD_ERROR.PRODUCT_NOT_FOUND,
+        message: "Product not found",
+      });
+    }
+
+    return res.status(200).json(products);
   }
 }
---
+++ san-font/src/pages/api/product/get-category/index.js
@@ -0,0 +1,25 @@
+import checkToken from "../../../../backend/authentication/auth.authentication.js";
+import pagingParse from "../../../../backend/middleware/paging.middleware.js";
+import db from "../../../../backend/models/index.js";
+import {
+  GLOBAL_STATUS,
+  GLOBAL_SWITCH,
+} from "../../../../backend/constants/common.constant.js";
+const { Op } = db.Sequelize;
+
+export default async function handler(req, res) {
+  pagingParse(req, res);
+//   checkToken(req, res);
+
+  if (req.method === "POST") {
+
+    const products = await db.ProductCategory.findOne({
+      where: { categorySlug : req.body.category_slug  },
+    
+    });
+
+   
+
+    return res.status(200).json({ products });
+  }
+}
